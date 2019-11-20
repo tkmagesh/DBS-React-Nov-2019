7d3a2a3fc555f776f36d4786ed22e536f92de222
Magesh Kuppan <tkmagesh77@gmail.com>
2019-11-20 14:34:00 +0800

    generalizing the creation of action dispatchers using 'bindActionCreators'

--- 04-state-manager/state-manager.js
+++ 04-state-manager/state-manager.js
@@ -29,5 +29,16 @@ let SM = (function(){
         return { getState, subscribe, dispatch };
     }
 
-    return { createStore };
+    function bindActionCreators(actionCreators, dispatch) {
+        let actionDispatchers = {};
+        for (let key in actionCreators) {
+            actionDispatchers[key] = function () {
+                let action = actionCreators[key].apply(undefined, arguments);
+                dispatch(action);
+            }
+        }
+        return actionDispatchers;
+    }
+
+    return { createStore, bindActionCreators };
 })();
